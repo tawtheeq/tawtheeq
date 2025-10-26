5572cbba934b3a0179528eac8ddfb8581567d668
maadiab <mohanad.dev@gmail.com>
2025-10-26 23:08:30 +0300

    change enpoint naming

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/maadiab/tawtheeq/tawtheeq/internal/handler"
-	"github.com/maadiab/tawtheeq/tawtheeq/internal/response"
 )
 
 func Router(h *handler.Handler) *http.ServeMux {
@@ -13,32 +12,15 @@ func Router(h *handler.Handler) *http.ServeMux {
 	// Define your routes here, for example:
 	// mux.HandleFunc("/api/users", handler.GetUsers)
 
-	mux.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			h.GetUsers(w, r) // يجيب كل المستخدمين
-		case http.MethodPost:
-			h.AddUser(w, r) //
-		default:
-			response.Error(w, http.StatusMethodNotAllowed, "Method not allowed")
-		}
+	mux.HandleFunc("GET /api/users", h.GetUsers)
+	mux.HandleFunc("GET /api/users/{id}", h.GetUserByID)
 
-	})
+	mux.HandleFunc("POST /api/users/", h.AddUser)
 
-	mux.HandleFunc("/api/users/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			h.GetUserByID(w, r)
-		case http.MethodPut:
-			h.UpdateUser(w, r)
-		default:
-			response.Error(w, http.StatusMethodNotAllowed, "Method not allowed")
-		}
-	})
-	mux.HandleFunc("/api/users", h.AddUser)
-	mux.HandleFunc("/api/users", h.UpdateUser)
+	mux.HandleFunc("PUT /api/users/{id}", h.UpdateUser)
+	mux.HandleFunc("DELETE /api/users/{id}", h.DeleteUser)
 
-	// mux.HandleFunc("GET /api/update-password", )
+	// mux.HandleFunc("GET /api/users/{id}/password", h.Upd)
 
 	mux.HandleFunc("GET /api/missions", h.GetAllMissions)
 
